70fe2ff3fe00eac78ff1ba3e40ec87e24d073951
Fernando Mendoza <fernandoza@gmail.com>
2017-04-09 19:23:38 -0500

    Fixed the dynamic load in the js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     console.log("Try the konami kode");
 
     $(".button-collapse").sideNav();
-    $('.modal').modal();
+    $(".modal").modal();
 
     var div_loading_progress = $(".div_loading_progress");
     var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], n = 0;
@@ -26,7 +26,7 @@ $(document).ready(function () {
     $(document).keydown(function (e) {
         if (e.keyCode === konamiCode[n++]) {
             if (n === konamiCode.length) {
-                console.log('Konami !!!');
+                console.log("Konami !!!");
                 konamiExecuted = true;
                 runIntro();
                 n = 0;
@@ -53,16 +53,18 @@ $(document).ready(function () {
         $("#overlay").fadeIn();
         $(".main-menu").removeClass("hide");
         $("#nav-btn").removeClass("menu-load menu-outro").addClass("menu-intro");
-        $('#nav-wrapper').removeClass("hide-on-large-only");
-        // $('.button-collapse').sideNav('show');
+        $("#nav-wrapper").removeClass("hide-on-large-only");
+        // $(".button-collapse").sideNav("show");
     });
 
     $(".links").click(function (event) {
-        $('.button-collapse').sideNav('hide');
-        var content = $('#content');
-        var target = '#' + $(this).data('target');
-        $(target).removeClass('hide');
-        $(content).html($(target));
+        $(".button-collapse").sideNav("hide");
+        var content = $("#content");
+        var targetText = "#" + $(this).data("target");
+        var targetDiv = $(targetText);
+        console.log(targetDiv);
+        // $(targetDiv).removeClass("hide");
+        $(content).html($(targetDiv).html());
     });
 
     $("#close-overlay").click(function () {
