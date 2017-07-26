82d9ef9dbb7fd9f7df60598085e62ca44d20245f
Fernando <fernandoza@gmail.com>
2017-07-26 17:55:26 -0500

    Fixs in CSS
    - Deleted some partial files, they were not being used.

--- php/mailer.php
+++ php/mailer.php
@@ -1,21 +1,21 @@
 <?php
-require 'PHPMailerAutoload.php';
+require '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'user@example.com';                 // SMTP username
-$mail->Password = 'secret';                           // SMTP password
+//$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'fer-mendoza.com;smtp.fer-mendoza.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = false;                               // Enable SMTP authentication
+$mail->Username = 'contact@fer-mendoza.com';                 // SMTP username
+$mail->Password = 'FerFurtad0..';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
 $mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addAddress('fernandoza@gmail.com', 'Joe User');     // Add a recipient
+$mail->addAddress('contact@fer-mendoza.com');               // Name is optional
 $mail->addReplyTo('info@example.com', 'Information');
 $mail->addCC('cc@example.com');
 $mail->addBCC('bcc@example.com');
