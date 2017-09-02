b870e54ba8657ae4901935b2295af96e82ac8041
Fernando <fernandoza@gmail.com>
2017-09-01 22:17:58 -0500

    Start the json structure for the projects info.

--- js/props.js
+++ js/props.js
@@ -1,6 +1,20 @@
 /**
  * Created by Fer on 3/11/17.
  */
+
+var projects = [
+    {
+        "name": "SA Taco Radar",
+        "url": "http://taco-radar.online",
+        "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
+    },
+    {
+        "name": "Project 2",
+        "url": "http://taco-radar.online",
+        "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
+    }
+];
+
 var PROPS = {
     experience: {
         java: {
