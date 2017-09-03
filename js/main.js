ce2346570670898e3a3bee807023a59eff1cad7d
Fernando <fernandoza@gmail.com>
2017-09-03 17:27:40 -0500

    Adds the images and their html structure in the js file.

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@ $(document).ready(function () {
     console.log("Try the konami kode");
     $('.modal').modal();
     $(".button-collapse").sideNav();
-    Materialize.fadeInImage('#content');
-    $('.tap-target').tapTarget('open');
-    $('.tap-target').tapTarget('close');
+    Materialize.fadeInImage("#content");
+    $('.tap-target').tapTarget("open").tapTarget("close");
 
     var div_loading_progress = $(".div_loading_progress");
     var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], n = 0;
@@ -19,8 +18,7 @@ $(document).ready(function () {
     function closeOverlay() {
         shrinkMenu();
         $("#overlay").fadeOut();
-        $("#nav-btn").removeClass("menu-intro");
-        $("#nav-btn").addClass("menu-outro");
+        $("#nav-btn").removeClass("menu-intro").addClass("menu-outro");
         $(".main-menu").fadeOut("menu-outro");
     }
 
@@ -80,8 +78,7 @@ $(document).ready(function () {
             growMenu();
         }, 1000);
 
-        $(".main-menu").fadeIn();
-        $(".main-menu").removeClass("hide");
+        $(".main-menu").fadeIn().removeClass("hide");
         $("#nav-btn").removeClass("menu-load menu-outro").addClass("menu-intro");
         $("#nav-wrapper").removeClass("hide-on-large-only");
         // $(".button-collapse").sideNav("show");
@@ -93,7 +90,7 @@ $(document).ready(function () {
         var targetText = "#" + $(this).data("target");
         var targetDiv = $(targetText);
 
-        if(targetText == "#contact"){
+        if(targetText === "#contact"){
             console.log($(this).children("a"));
             $(this).children("a")[0].click();
         }else{
@@ -108,5 +105,27 @@ $(document).ready(function () {
     $("#close-overlay").click(function () {
         closeOverlay();
     });
+
+    // Load projects section
+
+    var cardsDiv = $("#cards");
+    var cards = "";
+
+    PROPS.projects.forEach(function(el, i){
+        cards += '<div class="card">\n' +
+            '    <div class="card-image">\n' +
+            '        <img src="' + el.imgUrl + '">\n' +
+            '        <span class="card-title blue-text text-darken-1 blue lighten-5">' + el.name + '</span>\n' +
+            '    </div>\n' +
+            '    <div class="card-content">\n' +
+            '        <p>' + el.description + '</p>\n' +
+            '    </div>\n' +
+            '    <div class="card-action blue-text text-darken-1 ">\n' +
+            '        <a href="' + el.url + '" target="_blank">Link</a>\n' +
+            '    </div>\n' +
+            '</div>';
+    });
+
+    cardsDiv.append(cards);
 
 });
