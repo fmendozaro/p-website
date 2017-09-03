ee6ed40ddc7954ee94c416d8dcb69339d6af34fe
Fernando <fernandoza@gmail.com>
2017-09-03 17:27:57 -0500

    Details for including the image in each card.

--- js/props.js
+++ js/props.js
@@ -24,16 +24,19 @@ var PROPS = {
     "projects": [
         {
             "name": "SA Taco Radar",
+            "imgUrl": "./img/projects/taco.png",
             "url": "http://taco-radar.online",
             "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
         },
         {
             "name": "Nelly Furtado Mexico",
+            "imgUrl": "./img/bg1.jpg",
             "url": "http://nellyfurtado.com.mx",
             "description": "Website for the official mexican fan club for Nelly Furtado in Mexico."
         },
         {
             "name": "Loose Arts",
+            "imgUrl": "./img/projects/la.png",
             "url": "http://loosearts.com",
             "description": "A project made with one of my best friends to gather our freelance work between Mexico and Saudi Arabia."
         }
