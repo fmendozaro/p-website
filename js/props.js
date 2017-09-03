b32b349dfde3a45c48d66b7a98bd6e50b897fc1e
Fernando <fernandoza@gmail.com>
2017-09-03 16:25:33 -0500

    Updated the props file to json

--- js/props.js
+++ js/props.js
@@ -1,38 +1,41 @@
 /**
  * Created by Fer on 3/11/17.
  */
-
-var projects = [
-    {
-        "name": "SA Taco Radar",
-        "url": "http://taco-radar.online",
-        "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
-    },
-    {
-        "name": "Project 2",
-        "url": "http://taco-radar.online",
-        "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
-    }
-];
-
 var PROPS = {
-    experience: {
-        java: {
-            years: 3,
-            skills: ["Java SE, EE", "Spring Boot", "Hibernate", "Servlets", "JSP", "Thymeleaf"]
+    "experience": {
+        "java": {
+            "years": 3,
+            "skills": ["Java SE, EE", "Spring Boot", "Hibernate", "Servlets", "JSP", "Thymeleaf"]
         },
-        php:{
-            years: 5,
-            skills: ["Codeigniter", "Laravel"]
+        "php": {
+            "years": 5,
+            "skills": ["Codeigniter", "Laravel"]
         },
-        mysql:{
-            years: 8
+        "mysql": {
+            "years": 8
         },
-        sqlserver:{
-            years:2
+        "sqlserver": {
+            "years": 2
         },
-        grails:{
-            years:1
+        "grails": {
+            "years": 1
         }
-    }
-}
+    },
+    "projects": [
+        {
+            "name": "SA Taco Radar",
+            "url": "http://taco-radar.online",
+            "description": "Taco Radar a place where you can find the nearest and finest real mexican flavored tacos in San Antonio, Texas. If you ever wonder how a real taco taste like, you can give it a shot and see for yourself."
+        },
+        {
+            "name": "Nelly Furtado Mexico",
+            "url": "http://nellyfurtado.com.mx",
+            "description": "Website for the official mexican fan club for Nelly Furtado in Mexico."
+        },
+        {
+            "name": "Loose Arts",
+            "url": "http://loosearts.com",
+            "description": "A project made with one of my best friends to gather our freelance work between Mexico and Saudi Arabia."
+        }
+    ]
+};
