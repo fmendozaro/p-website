38b57716eb217173e4bd08901c35fc82c86941c6
Fernando <fernandoza@gmail.com>
2018-12-25 13:25:19 -0600

    feat: add sw

---
+++ fd/sw.js
@@ -0,0 +1,31 @@
+let CACHE_NAME = 'fd-v1';
+let urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/fd.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
+
