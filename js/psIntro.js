dd6253bd6859b81f172a42be9f6d051dc2114fc8
Fer <fernandoza@gmail.com>
2020-10-01 16:48:17 -0500

    feat: improves the UI/UX of the konami kode PS intro easter egg

--- js/psIntro.js
+++ js/psIntro.js
@@ -7,12 +7,12 @@ let psOne = $("#psone");
 let blackout = $("#blackout");
 
 function runIntro(){
-
     //Audio play
-    blackout.fadeIn();
+    blackout.show();
+    psOne.css('display', 'flex').hide().fadeIn(3000);
     $(".audio").trigger("play");
     psOne.addClass("psone-center");
-    psOne.fadeIn(2000);
+    psOne.fadeIn(3000);
 }
 
 function closeIntro(){
